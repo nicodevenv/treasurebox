6afb4f6b40753c5a9d01a875f9a28876b935f35c
Nicolas CHUNG <nido.dev.env@gmail.com>
2018-02-25 15:01:07 +0100

    1 service for each entities that have custom methods

--- src/Entities/Map.php
+++ src/Entities/Map.php
@@ -44,105 +44,6 @@ class Map {
         }
     }
 
-    public function getLongestCharCount($options)
-    {
-        $charCount = 0;
-        $reservedCharCount = 4;
-
-        foreach ($options as $option) {
-            $currentCharCount = 0;
-
-            if ($option instanceof TreasureOption) {
-                $currentCharCount = strlen($option->getCounter());
-            }
-            if ($option instanceof AdventurerOption) {
-                $currentCharCount = strlen($option->getName());
-            }
-
-            $currentCharCount += $reservedCharCount;
-            if ($currentCharCount > $charCount) {
-                $charCount = $currentCharCount;
-            }
-        }
-
-        return $charCount;
-    }
-
-    public function displayMap($longestCharCount)
-    {
-        $outputStr = '';
-        foreach ($this->mapFrames as $y => $row) {
-            foreach($row as $x => $data) {
-                $currentStr = '.';
-                if (count($data) > 0) {
-                    foreach ($data as $option) {
-                        if ($option instanceof MountainOption) {
-                            $currentStr = MountainOption::REFERENCE;
-                        }
-
-                        if ($option instanceof TreasureOption) {
-                            $currentStr = sprintf(
-                                '%s(%s)',
-                                TreasureOption::REFERENCE,
-                                $option->getCounter()
-                            );
-                        }
-
-                        if ($option instanceof AdventurerOption) {
-                            $currentStr = sprintf(
-                                '%s(%s)',
-                                AdventurerOption::REFERENCE,
-                                $option->getName()
-                            );
-                        }
-                    }
-                }
-
-                $missingBlankSpace = $longestCharCount - strlen($currentStr);
-                for ($i = 0; $i <= $missingBlankSpace; $i++) {
-                    $currentStr .= ' ';
-                }
-
-                $outputStr .= $currentStr;
-            }
-            $outputStr .= "\n";
-        }
-
-        return $outputStr;
-    }
-
-    /**
-     * @param AbstractOption $option
-     * @param bool $isStackable
-     *
-     * @throws \Exception
-     */
-    public function addOption($option, $isStackable = false)
-    {
-        if (!$isStackable && count($this->mapFrames[$option->getY()][$option->getX()]) > 0) {
-            throw new \Exception(
-                sprintf(
-                    'Unable to add %s.. another option is already using [%s,%s] position',
-                    $option->getType(),
-                    $option->getX(),
-                    $option->getY()
-                )
-            );
-        }
-
-        if ($isStackable && $this->isObstacleHere($option->getX(), $option->getY())) {
-            throw new \Exception(
-                sprintf(
-                    'Unable to put option in frame [%s, %s] because there is a mountain here.',
-                    $option->getX(),
-                    $option->getY()
-                )
-            );
-        }
-
-        $this->mapFrames[$option->getY()][$option->getX()][] = $option;
-    }
-
     public function getWidth()
     {
         return $this->mapWidth;
@@ -168,70 +69,13 @@ class Map {
         return $this->mapFrames;
     }
 
-    public function isAdventurerMovable(AdventurerOption $adventurer)
+    public function addOption($x, $y, AbstractOption $option)
     {
-        $nextPosition = $adventurer->getNextPosition();
-        if (
-            !CheckerService::isIntegerAndMoreThanZero(true, $nextPosition['x'], $this->getMaxX())
-            || !CheckerService::isIntegerAndMoreThanZero(true, $nextPosition['y'], $this->getMaxY())
-            || $this->isObstacleHere($nextPosition['x'], $nextPosition['y'])
-        ) {
-            return false;
-        }
-
-        return true;
+        $this->mapFrames[$y][$x][] = $option;
     }
 
-    public function isObstacleHere($x, $y) {
-        foreach ($this->mapFrames[$y][$x] as $option) {
-            if ($option instanceof MountainOption || $option instanceof AdventurerOption) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public function removeOption(AbstractOption $option)
+    public function removeOption($x, $y, $index)
     {
-        foreach ($this->mapFrames[$option->getY()][$option->getX()] as $index => $loopOption) {
-            if ($option === $loopOption) {
-                unset($this->mapFrames[$option->getY()][$option->getX()][$index]);
-            }
-        }
-
-    }
-
-    /**
-     * @param AdventurerOption $adventurer
-     * @throws \Exception
-     */
-    public function moveAdventurer(AdventurerOption $adventurer)
-    {
-        $this->removeOption($adventurer);
-        $adventurer->move();
-        $this->addOption($adventurer, true);
-
-        $this->collectSomething($adventurer);
-    }
-
-    /**
-     * @param AdventurerOption $adventurer
-     * @throws \Exception
-     */
-    private function collectSomething(AdventurerOption $adventurer)
-    {
-        if (count($this->mapFrames[$adventurer->getY()][$adventurer->getX()]) > 0) {
-            foreach ($this->mapFrames[$adventurer->getY()][$adventurer->getX()] as $optionIndex => $option) {
-                if ($option instanceof TreasureOption) {
-                    /** @var TreasureOption $option */
-                    $adventurer->addTreasure();
-                    $option->decrementCounter();
-                    if ($option->getCounter() === 0) {
-                        unset($this->mapFrames[$adventurer->getY()][$adventurer->getX()][$optionIndex]);
-                    }
-                }
-            }
-        }
+        unset($this->mapFrames[$y][$x][$index]);
     }
 }
